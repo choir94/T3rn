346ca00fe47ce9881df74b777107b5efaebc6588
Ahmad Choiruddin <83292508+choir94@users.noreply.github.com>
2024-09-11 05:57:55 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,8 +14,8 @@ cd $HOME
 sudo apt -q update
 sudo apt -qy upgrade
 
-EXECUTOR_URL="https://github.com/t3rn/executor-release/releases/download/v0.20.0/executor-linux-v0.20.0.tar.gz"
-EXECUTOR_FILE="executor-linux-v0.20.0.tar.gz"
+EXECUTOR_URL="https://github.com/t3rn/executor-release/releases/download/v0.21.0/executor-linux-v0.21.0.tar.gz"
+EXECUTOR_FILE="executor-linux-v0.21.0.tar.gz"
 
 echo "Downloading the Executor binary from $EXECUTOR_URL..."
 curl -L -o $EXECUTOR_FILE $EXECUTOR_URL
